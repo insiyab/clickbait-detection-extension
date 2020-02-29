113b31871206b91a13efc9201e5d98aad31f181b
Daniel Tisdale <dctisdal@calpoly.edu>
2020-02-29 15:27:12 -0800

    added entitiy analysis

--- lang_parse.py
+++ lang_parse.py
@@ -4,6 +4,11 @@ import sys
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+
+
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+
 # Instantiates a client
 client = language.LanguageServiceClient()
 
@@ -32,8 +37,58 @@ def classify(text, verbose=True):
             print(u'=' * 20)
             print(u'{:<16}: {}'.format('category', category.name))
             print(u'{:<16}: {}'.format('confidence', category.confidence))
+        print("\n")
+
+
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'California is a state.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = enums.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language2 = "en"
+    document = {"content": text, "type": type_, "language": language2}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = enums.EncodingType.UTF8
+
+    response = client.analyze_entities(document, encoding_type=encoding_type)
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(enums.Entity.Type(entity.type).name))
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print(u"Mention text: {}".format(mention.text.content))
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                u"Mention type: {}\n".format(enums.EntityMention.Type(mention.type).name)
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
 
     return result
+
+
 
 if __name__ == '__main__':
 	classify(sys.argv[1])
