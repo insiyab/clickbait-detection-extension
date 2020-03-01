b8d5ddec349a6992e73fffa2485ed5fdcf23adf9
Insiya <insiyab8@gmail.com>
2020-03-01 04:11:33 -0800

    Backend handles audio & video analysis upon GET request

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from predict_youtube import *
 import json
+import os
 
 app = Flask(__name__)
 
@@ -12,13 +13,15 @@ def hello_world():
 def check_for_clickbait():
     client_input = request.json
 
-    title, views, likes, dislikes = None, None, None, None
+    # create base response
     response = {
         "success": False,
         "is_clickbait": -1,
         "error": "INVALID_REQUEST"
     }
 
+    # get parameters from JSON request
+    title, views, likes, dislikes = None, None, None, None
     if 'title' in client_input:
         title = str(client_input['title'])
     else:
@@ -30,6 +33,7 @@ def check_for_clickbait():
     if 'dislikes' in client_input:
         dislikes = int(client_input['dislikes'])
 
+    # analyze parameters and send JSON response
     try:
         response["is_clickbait"] = youtube_predictor(title, views, likes, dislikes, None)
         response["success"] = True
@@ -47,4 +51,53 @@ def check_for_clickbait():
 @app.route('/get_info', methods = ['GET'])
 def get_more_info():
     client_input = request.json
-    return "getting more information"
+
+    # create base response
+    response = {
+        "success": False,
+        "audio_analysis": -1,
+        "visual_analysis": -1,
+        "error": "INVALID_REQUEST"
+    }
+
+    # get parameters from JSON request
+    URL, audio_transcription = None, None
+
+    if 'URL' in client_input:
+        URL = str(client_input['URL'])
+    else:
+        return json.dumps(response), 400
+
+    if 'audio_transcription' in client_input:
+        audio_transcription = str(client_input['audio_transcription'])
+    else:
+        response["error"] = "MISSING_AUDIO_TRANSCRIPTION"
+        return json.dumps(response), 400
+
+    # download YouTube video to vid_shots
+    try:
+        os.system("cd vid_shots && rm -rf *.mp4 && youtube-dl --no-check-certificate " + URL)
+    except Exception as err:
+        print('Error:', err)
+        response["success"] = False
+        response["audio_analysis"] = -1
+        response["video_analysis"] = -1
+        response["error"] = "VIDEO_RETRIEVAL_FAILED"
+        return json.dumps(response), 400
+
+    # analyze video content and send JSON response
+    try:
+        response["audio_analysis"] = {} # call appropriate function
+        response["video_analysis"] = {} # call appropriate function
+        response["success"] = True
+        response["error"] = "None"
+        return response
+    except Exception as err:
+        print('Error:', err)
+        response["success"] = False
+        response["audio_analysis"] = -1
+        response["video_analysis"] = -1
+        response["error"] = "ANALYSIS_FAILED"
+        return json.dumps(response), 400
+
+    # return "getting more information"
